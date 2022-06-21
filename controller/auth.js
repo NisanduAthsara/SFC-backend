00c0ec7de55738d27ed549c9126d64e6d388ea58
NisanduAthsara <nisandunox@gmail.com>
2022-06-21 13:46:02 +0530

    Update security of the backend

--- controller/auth.js
+++ controller/auth.js
@@ -100,3 +100,52 @@ exports.authMiddleware = (req,res,next)=>{
         })
     }
 }
+
+exports.checkToken = async (req,res)=>{
+    const token = req.body.token
+    if(!token){
+        return res.json({
+            success:false,
+            message:"Unauthorized User...!"
+        })
+    }
+    let decodeToken
+    try{
+        decodeToken = jwt.verify(token,process.env.TOKEN)
+    }catch(err){
+        return res.json({
+            success:false,
+            message:"Unauthorized User...!"
+        })
+    }
+
+    if(!decodeToken.id){
+        return res.json({
+            success:false,
+            message:"Unauthorized User...!"
+        })
+    } 
+
+
+    try {
+        const user = await User.findById(decodeToken.id)
+        if(!user){
+            return res.json({
+                success:false,
+                message:"Unauthorized User...!"
+            })
+        }
+
+        return res.json({
+            success:true,
+            message:"Authorized User...!"
+        })
+
+    } catch (error) {
+        return res.json({
+            success:false,
+            message:"Something went wrong...!"
+        })
+    }
+
+}
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {signup,login} = require('../controller/user.loginSignup')
-const {getUserId,getUserById,authMiddleware} = require('../controller/auth')
+const {getUserId,getUserById,authMiddleware,checkToken} = require('../controller/auth')
 
 router.post('/signup',signup)
 router.post('/login',login)
 router.post('/getUserId',authMiddleware,getUserId)
 router.post('/getUser',authMiddleware,getUserById)
+router.post('/checkToken',checkToken)
 
 module.exports = router
