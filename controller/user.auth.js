b1b8c346bc6fadf30b2ea74814680e5e8da46a44
NisanduAthsara <nisandunox@gmail.com>
2022-06-27 19:00:47 +0530

    Add another auth route

--- controller/user.auth.js
+++ controller/user.auth.js
@@ -252,3 +252,60 @@ exports.checkUserToken = async (req,res)=>{
     }
 
 }
+
+
+exports.checkSellerToken = async (req,res)=>{
+    const token = req.body.token
+    if(!token){
+        return res.json({
+            success:false,
+            message:"Unauthorized User...!"
+        })
+    }
+    let decodeToken
+    try{
+        decodeToken = jwt.verify(token,process.env.TOKEN)
+    }catch(err){
+        return res.json({
+            success:false,
+            message:"Unauthorized User...!"
+        })
+    }
+
+    if(!decodeToken.id){
+        return res.json({
+            success:false,
+            message:"Unauthorized User...!"
+        })
+    } 
+
+
+    try {
+        const user = await User.findById(decodeToken.id)
+        if(!user){
+            return res.json({
+                success:false,
+                message:"Unauthorized User...!"
+            })
+        }
+
+        if(user.accountType !== 'Seller'){
+            return res.json({
+                success:false,
+                message:"Unauthorized User...!"
+            })
+        }
+
+        return res.json({
+            success:true,
+            message:"Authorized User...!"
+        })
+
+    } catch (error) {
+        return res.json({
+            success:false,
+            message:"Something went wrong...!"
+        })
+    }
+
+}
